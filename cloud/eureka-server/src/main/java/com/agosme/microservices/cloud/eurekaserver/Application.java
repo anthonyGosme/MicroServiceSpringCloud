47fa92dacf46786b3d3a3d78d5095bbf4403fa9b
AnthonyGosme <anthonygosme@gmail.com>
2020-04-16 19:45:28 +0200

    convet core API getter to non blocking webflux

---
+++ cloud/eureka-server/src/main/java/com/agosme/microservices/cloud/eurekaserver/Application.java
@@ -0,0 +1,13 @@
+package com.agosme.microservices.cloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
